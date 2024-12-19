0e93869f30b75c37ace209ddea4b72c5a2076527
Limbo <hoolimbo@gmail.com>
2024-12-19 18:45:26 +0100

    update

--- src/page.tsx
+++
@@ -1,9 +0,0 @@
-import { MilanTransitDashboard } from '@/components/MilanTransitDashboard';
-
-export default function Home() {
-  return (
-    <main className="min-h-screen">
-      <MilanTransitDashboard />
-    </main>
-  );
-}
