e8b6cb8632656d6214b904e6095f76aeeda03aa3
Limbo <hoolimbo@gmail.com>
2024-12-19 19:05:51 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import { MilanTransitDashboard } from '@/components/MilanTransitDashboard';
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 export default function Home() {
   return (
     <main className="min-h-screen bg-gray-100">
       <MilanTransitDashboard />
+	<SpeedInsights />
     </main>
   );
 }
