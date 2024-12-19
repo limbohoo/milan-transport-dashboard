1b386518962fce499b2a19dde4b4661b8f985a9f
Limbo <hoolimbo@gmail.com>
2024-12-19 18:48:42 +0100

    Update card.tsx

---
+++ src/components/ui/card.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+interface CardProps {
+  className?: string;
+  children?: React.ReactNode;
+}
+
+export function Card({ className = "", children }: CardProps) {
+  return (
+    <div
+      className={`rounded-lg border bg-white shadow-sm ${className}`}
+    >
+      {children}
+    </div>
+  );
+}
